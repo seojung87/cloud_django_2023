0076ee94ffbc374a1d7231e3b0af586a727767de
seojungYoo <seojungyoo22@gmail.com>
2023-03-21 16:55:32 +0900

    블로그 목록 뷰를 만들었다네

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')  #DB에서 모든 목록 가져오기
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,     #context - 글 리스트 통째로 넘겨줌
+        }
+    )
