864f83b06981e738c82da098bb5bdabc81ae4b9b
seojungYoo <seojungyoo22@gmail.com>
2023-03-28 15:40:10 +0900

    CBV refactoring

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:post_num>/', views.single_post_page),
-    path('', views.index),
+    #path('<int:post_num>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    #path('', views.index),
+    path('', views.PostList.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,35 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')  #DB에서 모든 목록 가져오기
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')  #DB에서 모든 목록 가져오기
+#
+#    return render(
+#        request,
+#        'blog/post_list.html',
+#        {
+#            'posts': posts,     #context - 글 리스트 통째로 넘겨줌
+#        }
+#    )
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,     #context - 글 리스트 통째로 넘겨줌
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
+class PostDetail(DetailView):
+    model = Post
+
+'''
 def single_post_page(request, post_num):
     post = Post.objects.get(pk=post_num)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post': post,
         }
     )
+'''
