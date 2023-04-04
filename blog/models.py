ded0c07f7a273fa36cddc48aaf4ecc1ea59d3796
seojungYoo <seojungyoo22@gmail.com>
2023-04-04 15:30:48 +0900

    author 추가

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
-import os.path
-
 from django.db import models
+from django.contrib.auth.models import User
+import os.path
 
 # Create your models here.
 class Post(models.Model):
@@ -13,8 +13,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     def __str__(self):
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
